5bd0e9c24e41fb24dec6a3605ff15905393a42f7
Alex-GHP <alex.morariu.dev@gmail.com>
2025-02-14 15:06:22 +0200

    final touches and finish logic

--- src/get_work_days.py
+++ src/get_work_days.py
@@ -1,5 +1,6 @@
 from constants import MIN_LEGAL_WORK_DAYS, MAX_LEGAL_WORK_DAYS, WEEKEND_DAYS
 import csv
+from days import *
 
 def get_user_working_days(shifts):
     count = 0
@@ -8,6 +9,14 @@ def get_user_working_days(shifts):
         if shift.upper() in valid_shifts:
             count += 1
     return count
+
+def get_day_object(day, shift, hourly_rate):
+    weekdays = ["monday", "mondayph", "tuesday", "tuesdayph", "wednesday", "wednesdayph", "thursday", "thursdayph"]
+    weekend_days = ["friday", "fridayph", "saturday", "saturdayph", "sunday", "sundayph"]
+
+    if day in weekdays:
+        return Weekday(day, shift, hourly_rate)
+    return WeekendDay(day, shift, hourly_rate)
 
     
         
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import csv
 
 from constants import BASE_SALARY, PAID_HOURS_PER_SHIFT, WEEKEND_DAYS
-from get_work_days import get_user_working_days
+from get_work_days import get_user_working_days, get_day_object
+from days import *
 
 
 with open("Schedule - 2025.csv", mode="r") as file:
@@ -14,18 +15,24 @@ with open("Schedule - 2025.csv", mode="r") as file:
 work_days = len(days) - 1 - WEEKEND_DAYS
 user_working_days = get_user_working_days(shifts)
 
-print(user_working_days)
-
-total_salary = 0
-for day in days[1:]:
-    work_day = get_day_object(day)
-    total_salary += work_day.get_money()
+print(days)
+print(shifts)
 
 daily_rate = BASE_SALARY // work_days
 hourly_rate = daily_rate / PAID_HOURS_PER_SHIFT
 
-def get_hours(day, shift):
-    pass
+total_salary = 0
+for i in range(1, len(days)):
+    if shifts[i] == "OFF":
+        continue
+    work_day = get_day_object(days[i], shifts[i], hourly_rate)
+    if "ph" in work_day.day:
+        total_salary += work_day.get_money() * 2
+    total_salary += work_day.get_money()
+
+print(total_salary)
+
+
 
 
 
